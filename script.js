4f6ab5e7d0cfd5edab6cca45239b5bc182676ba3
Guga <gustavo.ferreira0821@gmail.com>
2023-03-09 23:47:20 -0300

    :wrench: chore: more time in carousel

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+});
+
+var myCarousel = document.querySelector('#carouselExampleCaptions')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 3000,
+  wrap: true
+});
+
